e412c136e18891c11c18e9cff3ae6b0954918376
Yufei Zheng <yufeiz2@houndshark.ics.cs.cmu.edu>
2021-11-06 22:04:22 -0400

    finish exercise2 cache awared algorithm

--- pi.c
+++ pi.c
@@ -17,23 +17,31 @@ History: Written by Tim Mattson, 11/99.
 #include <omp.h>
 static long num_steps = 100000000;
 double step;
+#define NUMS_THREADS 8
+#define PAD 8
 int main ()
 {
-	  int i;
-	  double x, pi, sum = 0.0;
-	  double start_time, run_time;
-
-	  step = 1.0/(double) num_steps;
-
-        	 
-	  start_time = omp_get_wtime();
-
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
+	int i, nthreads;
+	double  pi, sum[NUMS_THREADS][PAD];
+	double start_time, run_time;
+	omp_set_num_threads(NUMS_THREADS);
+	step = 1.0/(double) num_steps;
+	start_time = omp_get_wtime();
+	#pragma omp parallel
+	{
+	  int i, id, nthrds;
+	  double x;
+	  id = omp_get_thread_num();	  
+	  nthrds = omp_get_num_threads();
+	  if(id == 0) nthreads = nthrds;
+	  for (i=id, sum[id][0] = 0.0;i< num_steps; i = i + nthrds){
+		  x = (i+0.5)*step;
+		  sum[id][0] += 4.0/(1.0+x*x);
 	  }
-
-	  pi = step * sum;
+	}
+	for(i = 0;i<NUMS_THREADS;i++)
+		pi += sum[i][0];
+	  // pi = step * sum;
 	  run_time = omp_get_wtime() - start_time;
 	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
 }	  
